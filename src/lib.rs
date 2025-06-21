37efe33365d739ad36bb249764ca5dde843f2022
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-06-21 15:18:18 +0800

    UPDATE

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-mod str;
-mod varint;
-
-pub use str::encode_str_from_slice;
-pub use str::encode_str_from_heap;
 pub use str::decode_str;
-
+pub use str::encode_str_from_heap;
+pub use str::encode_str_from_slice;
 pub use varint::create_varint;
 pub use varint::decode_varint;
+
+mod str;
+mod varint;
